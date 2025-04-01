5b6b9176cb12fdf4d75cc1facc892d6cdb78cd5b
Laziz Ays <62626657+Laziz10@users.noreply.github.com>
2025-03-31 21:42:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,26 @@ with st.sidebar:
         st.session_state.selected_speaker = selected_speaker
         st.session_state.selected_benchmark = None
 
+    if view_mode in ["Benchmark Analysis", "Risk Analysis"]:
+    st.markdown(f"<div style='{sidebar_header_style}'>{view_mode}</div>", unsafe_allow_html=True)
+    selected_benchmark = st.selectbox(
+        label="Benchmark Dropdown",
+        options=benchmark_stocks,
+        index=benchmark_stocks.index(st.session_state.selected_benchmark) if st.session_state.selected_benchmark else 0,
+        label_visibility="collapsed",
+        key="benchmark_dropdown"
+        )
+    st.session_state.selected_benchmark = selected_benchmark
+    st.session_state.selected_speaker = "All"
+    
+    if st.session_state.uploaded_file is None:
+        st.markdown("### **Upload Earnings Call PDF**", unsafe_allow_html=True)
+        uploaded = st.file_uploader("", type=["pdf"], key="uploader")
+
+    if uploaded is not None:
+       st.session_state.uploaded_file = uploaded
+       st.rerun()
+
 if view_mode == "Market Analysis":
     st.markdown("### Market Analysis")
 
@@ -237,25 +257,6 @@ Answer:
         except Exception as e:
             st.error(f"Error fetching data: {e}")
 
-if view_mode in ["Benchmark Analysis", "Risk Analysis"]:
-    st.markdown(f"<div style='{sidebar_header_style}'>{view_mode}</div>", unsafe_allow_html=True)
-    selected_benchmark = st.selectbox(
-        label="Benchmark Dropdown",
-        options=benchmark_stocks,
-        index=benchmark_stocks.index(st.session_state.selected_benchmark) if st.session_state.selected_benchmark else 0,
-        label_visibility="collapsed",
-        key="benchmark_dropdown"
-        )
-    st.session_state.selected_benchmark = selected_benchmark
-    st.session_state.selected_speaker = "All"
-    
-    if st.session_state.uploaded_file is None:
-        st.markdown("### **Upload Earnings Call PDF**", unsafe_allow_html=True)
-        uploaded = st.file_uploader("", type=["pdf"], key="uploader")
-
-    if uploaded is not None:
-       st.session_state.uploaded_file = uploaded
-       st.rerun()
 
 # --- Main Header ---
 st.image("vanguard_logo.png", width=180)  
